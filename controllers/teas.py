435f08f89b3363f533fc0f42b975569438b34575
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-24 10:34:14 +0300

    Add HTTPException handling for tea retrieval by ID

--- controllers/teas.py
+++ controllers/teas.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from models.tea_data import teas_db
 
 router = APIRouter()
@@ -7,3 +7,13 @@ router = APIRouter()
 @router.get("/teas")
 def get_teas():
     return teas_db
+
+
+@router.get("/teas/{tea_id}")
+def get_single_tea(tea_id: int):
+    # Get tea by ID
+    for tea in teas_db["teas"]:
+        if tea["id"] == tea_id:
+            return tea
+    # If tea with the given ID is not found
+    raise HTTPException(status_code=404, detail="Tea not found")
